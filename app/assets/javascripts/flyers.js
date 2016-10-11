f069e5bc879cd9efa432e82807e664fbf97451fa
daynearron <dwatai@commercemtg.com>
2016-10-11 11:31:39 -0700

    fixed thumb layout

--- app/assets/javascripts/flyers.js
+++ app/assets/javascripts/flyers.js
@@ -1,12 +1,10 @@
+// $(function(){
+//   var $container = $('#masonry-container');
+//   $container.imagesLoaded( function() {
 
-$(function(){
-
-  var $container = $('#masonry-container');
-  $container.imagesLoaded( function() {
-
-    $container.masonry({
-        itemSelector : '.thumb',
-        originLeft: true
-    });    
-  });
-});
+//     $container.masonry({
+//         itemSelector : '.thumb',
+//         originLeft: true
+//     });    
+//   });
+// });
