65a0d924b62ce0962c62199de337db643a67d6e8
daynearron <dwatai@commercemtg.com>
2016-10-13 15:32:29 -0700

    scroll to top, logo on login and edit/delete flyer

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,25 @@
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 200){ 
-    $('.hero-wrap-js').addClass("smaller");
-  }
-  else{
-    $('.hero-wrap-js').removeClass("smaller");
-  }
+$(function(){
+	$(window).scroll(function() {
+	  if ($(this).scrollTop() > 200){ 
+	    $('.hero-wrap-js').addClass("smaller");
+	  }
+	  else{
+	    $('.hero-wrap-js').removeClass("smaller");
+	  }
+	});
+
+	$(window).scroll(function(){
+	    if ($(this).scrollTop() > 600) {
+	        $(".scrollToTop").fadeIn(1000)
+	    } else {
+	        $(".scrollToTop").fadeOut(1000);
+	    }
+	});
+
+	//Click event to scroll to top
+	$(".scrollToTop").click(function(){
+	    $('html, body').animate({scrollTop : 0},500);
+	    return false;
+	});
 });
+	
