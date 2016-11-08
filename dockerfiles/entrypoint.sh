4266bc8ebfb20b9daf9fdcc7a65a8763995aced6
Chris Volk <cvolk@commercemtg.com>
2016-11-08 13:31:40 -0800

    Enhance entrypoint such that the db is fully ready before the web process starts

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,7 +1,24 @@
 #!/bin/bash 
 # 
+DB_READY=1
+DB_CHECK_COUNT=0
+DB_MAX_CHECKS=15
 
 if [ $# -eq 0 ]; then
+
+     # Wait for db to be fully ready, with timeout
+     while [ $DB_READY -ne 0 ] 
+     do 
+        sleep 2
+        netcat -z db 5432
+        DB_READY=$?
+        ((DB_CHECK_COUNT++))
+        if [ $DB_CHECK_COUNT -gt $DB_MAX_CHECKS ]
+        then
+         exit 1
+        fi
+     done
+
      foreman run bundle exec rake db:create     
      foreman run bundle exec rake db:migrate     
      foreman run bundle exec rake db:seed  
