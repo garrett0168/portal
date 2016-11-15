625df8bccb90ca9221aeb751c02d1b5fd4a37467
Chris Volk <cvolk@commercemtg.com>
2016-11-15 13:19:54 -0800

    fix multiple bugs that were clobbering db tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.destroy_all
+if Category.count == 0
+    category = Category.create(name: 'Company')
 
+    category = Category.create(name: 'Product')
+    category.children.create(name: 'Conventional')
+    category.children.create(name: 'FHA')
+    category.children.create(name: 'HELOC')
+    category.children.create(name: 'First Time Homebuyer')
+    category.children.create(name: 'Homebuyer')
+    category.children.create(name: 'Jumbo')
+    category.children.create(name: 'Refinance')
+    category.children.create(name: 'Reverse')
+    category.children.create(name: 'USDA')
+    category.children.create(name: 'VA')
 
+    category = Category.create(name: 'Partners')
+    category.children.create(name: 'Referrals')
+    category.children.create(name: 'Co-Branded')
 
-category = Category.create(name: 'Company')
+    category = Category.create(name: 'Recruiting')
+    category = Category.create(name: 'Spanish')
+    category = Category.create(name: 'Sales Presentations')
+end
 
-category = Category.create(name: 'Product')
-category.children.create(name: 'Conventional')
-category.children.create(name: 'FHA')
-category.children.create(name: 'HELOC')
-category.children.create(name: 'First Time Homebuyer')
-category.children.create(name: 'Homebuyer')
-category.children.create(name: 'Jumbo')
-category.children.create(name: 'Refinance')
-category.children.create(name: 'Reverse')
-category.children.create(name: 'USDA')
-category.children.create(name: 'VA')
-
-category = Category.create(name: 'Partners')
-category.children.create(name: 'Referrals')
-category.children.create(name: 'Co-Branded')
-
-category = Category.create(name: 'Recruiting')
-category = Category.create(name: 'Spanish')
-category = Category.create(name: 'Sales Presentations')
-
-User.create!(email: "test@test.com", password: "123456", password_confirmation: "123456", admin: true)
+if User.count == 0
+    User.create!(email: "test@test.com", password: "123456", password_confirmation: "123456", admin: true)
+end
